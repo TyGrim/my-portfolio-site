8c085b80204953720aa190d3953109961a1a9dfa
Tybo <tysongyorfy@gmail.com>
2023-08-17 11:13:29 -0700

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx"],
+  mode: "jit", /* Just in Time */
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#070818",
+        secondaryColor: "#ada8c5",
+        accent: "#171032",
+        "dark-100": "110f27",
+        "dark-200": "#0b0527",
+        "light-gray": "#f5f5f5",
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      boxShadow: {
+        customBox: "0px 30px 110px -10px #221f37"
+      },
+      screens: {
+        mini: "450px",
+      },
+      backgroundImage: {
+        "pattern-hero": " placeholder "
+      }
+    },
   },
   plugins: [],
 }
